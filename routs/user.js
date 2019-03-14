6c76eaaf2d9755f19984d53ae6d20a0781dd72cf
zenitfz@gmail.com <8SkKQxZr>
2019-03-14 05:24:21 +0400

    oi

--- routs/user.js
+++ routs/user.js
@@ -51,12 +51,12 @@ router.put('/login', async (req, res) => {
             if (!user || !(await hash.compar(req.body.password, user.password))) {
                 res.status(401).send('user is not authorised'); //todo проверить статус
             } else {
-                console.log(res)
                 res.send({
                     jwt: jwt.sign({name: user.name, roles: user.roles, _id: user._id}, SECRET),
                     name: user.name
             })
                 ;//то что мы подписываем
+                console.log(jwt.sign({name: user.name, roles: user.roles, _id: user._id}, SECRET))
             }
         } catch (e) {
             res.send(e)
